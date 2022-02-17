7c22caa7ef2c87498e8cae6f08cf199371b4b338
fedor <fedor@email.com>
2022-02-17 06:08:08 +0300

    now it is possible to add movies to the page

--- js/script.js
+++ js/script.js
@@ -1,50 +1,93 @@
-/* Задания на урок:
+/* 
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
+5) Фильмы должны быть отсортированы по алфавиту */
 
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+    
+    const adv = document.querySelectorAll('.promo__adv img'),
+          poster = document.querySelector('.promo__bg'),
+          genre = document.querySelector('.promo__genre'),
+          movieList = document.querySelector('.promo__interactive-list'),
+          addForm = document.querySelector('form.add'),
+          inputForm = addForm.querySelector('.adding__input'),
+          deleteMovi = document.querySelectorAll('delete'),
+          checkbox = addForm.querySelector('[type="checkbox"]');
+
+        addForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const newFilm = inputForm.value;
+        const favorite = checkbox.checked;
+        /* if (newFilm.leght > 21 ) {
+            newFilm = newFilm.substring(0, 20) + '...';
+        } */
+        movieDB.movies.push(newFilm);
+        sortArr(movieDB.movies);
+    
+        createMoviList(movieDB.movies, movieList);
+    
+        event.target.reset();
+    });
+        
+    const deleteAdv = (arr) => {
+        arr.forEach(item => {
+            item.remove();
+        });
+    }
+    deleteAdv(adv);
+    
+    const makeChanges = (genre, poster) => {
+        genre.textContent = 'драма';
+    
+        poster.style.backgroundImage = 'url("img/bg.jpg")';
+    }
+
+    makeChanges(genre, poster);
+
+    const  sortArr = (arr) => {
+        arr.sort();
+    }
+    
 
 
-const promoAdv = document.querySelectorAll('.promo__adv img'),
-      promoGenre = document.querySelector('.promo__genre'),
-      promoBg = document.querySelector('.promo__bg'),
-      promoItem = document.querySelectorAll('.promo__interactive-item');
 
+        function createMoviList(films, parent) {
+            parent.innerHTML = "";
 
-// 1)Первое задание, в котором мы изменяем текст
-for(let i = 0; i < promoAdv.length; i++) {
-    promoAdv[i].remove();
-}
+            films.forEach((film, i) => {
+                parent.innerHTML += `
+                    <li class="promo__interactive-item">${i + 1} ${film}
+                        <div class="delete"></div>
+                    </li>
+                `;
+            });
+        }
 
-// 2)Втрое задание, в котором мы изменяем комедия - драма
-promoGenre.innerHTML = 'драма'; 
-
-// 3)Третье задание, в котором мы изменяем фон блока
-promoBg.style.backgroundImage = 'url(../img/bg.jpg)';
-
-// 4-5)Сортировка фильмов на странице  и добавлнение номирации к фильмам
-movieDB.movies.sort();
-for (let i = 0; i < movieDB.movies.length; i++) {
-    promoItem[i].innerHTML = ( i+1 + ') ') + movieDB.movies[i];
-}
+        deleteAdv(adv);
+        makeChanges(genre, poster);
+        sortArr(movieDB.movies);
+        createMoviList(movieDB.movies, movieList);
+});
 
